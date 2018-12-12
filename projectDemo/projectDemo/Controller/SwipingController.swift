2ffdc57d3f414be0a368750cd35106ef806c9886
MAC <>
2018-12-12 21:34:40 +0700

    Create Generics TableView

---
+++ projectDemo/projectDemo/Controller/SwipingController.swift
@@ -0,0 +1,17 @@
+//
+//  SwipingController.swift
+//  projectDemo
+//
+//  Created by Hai Vo L. on 12/12/18.
+//  Copyright © 2018 Hai Vo L. All rights reserved.
+//
+
+import UIKit
+
+final class SwipingController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
