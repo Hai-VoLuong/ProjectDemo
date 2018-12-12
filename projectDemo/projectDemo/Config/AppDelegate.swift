460e2e5c36f320b3a001779dcbe06441ab6c5ddd
MAC <>
2018-12-13 01:01:59 +0700

    Create Generics Collection View

--- projectDemo/projectDemo/Config/AppDelegate.swift
+++ projectDemo/projectDemo/Config/AppDelegate.swift
@@ -17,7 +17,49 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        window?.rootViewController = UINavigationController(rootViewController: MainController())
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        window?.rootViewController = CatController(collectionViewLayout: layout)
         return true
+    }
+}
+
+struct Person {
+    let firstName: String
+    let lastName: String
+}
+
+class CatCell: GenericCollectionCell<Person> {
+    
+    let textLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        backgroundColor = .red
+        addSubview(textLabel)
+        textLabel.fillSuperview()
+    }
+    
+    override var item: Person! {
+        didSet {
+            textLabel.text = item.firstName
+        }
+    }
+}
+
+class CatController: GenericCollecitonView<CatCell, Person>, UICollectionViewDelegateFlowLayout {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        items = [Person(firstName: "Bill", lastName: "Clinton"),
+                 Person(firstName: "Barack", lastName: "Obama")]
+        collectionView.backgroundColor = .white
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 50)
     }
 }
