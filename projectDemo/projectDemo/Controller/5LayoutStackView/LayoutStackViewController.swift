537d2d33308141cfc39ebd0db9eb260090210b15
MAC <>
2018-12-18 22:48:54 +0700

    Preparing init

---
+++ projectDemo/projectDemo/Controller/5LayoutStackView/LayoutStackViewController.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  projectDemo
+//
+//  Created by MAC on 12/18/18.
+//  Copyright © 2018 Hai Vo L. All rights reserved.
+//
+
+import UIKit
+
+final class LayoutStackViewController: UIViewController {
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Lay out Stack View"
+        view.backgroundColor = .white
+    }
+}
