aeff44ee6cc3d13420b8b5721e175b5c416b9f02
Hai Vo L <hai.vo-fr@asiantech.vn>
2018-12-19 14:06:34 +0700

    Preparing init

---
+++ projectDemo/projectDemo/Controller/7SwipePage/SwipePageController.swift
@@ -0,0 +1,18 @@
+//
+//  SwipePageController.swift
+//  projectDemo
+//
+//  Created by Hai Vo L. on 12/19/18.
+//  Copyright © 2018 Hai Vo L. All rights reserved.
+//
+
+import UIKit
+
+final class SwipePageController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Swipe Pages"
+        view.backgroundColor = .white
+    }
+}
