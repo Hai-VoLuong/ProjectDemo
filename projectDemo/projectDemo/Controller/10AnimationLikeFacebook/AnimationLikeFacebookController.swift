3a6a55232578399880495f1ab0a3f58b247e2ec1
MAC <>
2018-12-22 21:32:07 +0700

    Preparing init

---
+++ projectDemo/projectDemo/Controller/10AnimationLikeFacebook/AnimationLikeFacebookController.swift
@@ -0,0 +1,18 @@
+//
+//  AnimationLikeFacebook.swift
+//  projectDemo
+//
+//  Created by MAC on 12/22/18.
+//  Copyright © 2018 Hai Vo L. All rights reserved.
+//
+
+import UIKit
+
+final class AnimationLikeFacebookController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Animation Like Facebook"
+        view.backgroundColor = .white
+    }
+}
