f13e45f98ccbf0efdc003a149c141ac577d09497
haivl <hai.vl@techchain.vn>
2019-01-12 15:41:56 +0700

    show image into cell message controller

--- projectDemo/projectDemo/Controller/14firebaseMessage/MessageController.swift
+++ projectDemo/projectDemo/Controller/14firebaseMessage/MessageController.swift
@@ -11,16 +11,64 @@ import Firebase
 
 final class MessageCell: BaseTableCell<Message> {
     
+    private let titleLabel: UILabel = {
+        let l = UILabel()
+        return l
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let l = UILabel()
+        l.font = UIFont.systemFont(ofSize: 12)
+        return l
+    }()
+    
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "course_banner")
+        iv.layer.cornerRadius = 25
+        iv.clipsToBounds = true
+        return iv
+    }()
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(top: nil, leading: leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: 0, left: 8, bottom: 0, right: 0), size: .init(width: 50, height: 50))
+        profileImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        addSubview(titleLabel)
+        titleLabel.anchor(top: topAnchor, leading: profileImageView.trailingAnchor, bottom: nil, trailing: nil, padding: .init(top: 8, left: 8, bottom: 0, right: 0), size: .init(width: titleLabel.frame.width, height: titleLabel.frame.height))
+        //titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        addSubview(descriptionLabel)
+        descriptionLabel.anchor(top: titleLabel.bottomAnchor, leading: profileImageView.trailingAnchor, bottom: nil, trailing: nil, padding: .init(top: 0, left: 8, bottom: 0, right: 0), size: .init(width: descriptionLabel.frame.width, height: descriptionLabel.frame.height))
+        
+    }
+
+
     override var item: Message! {
         didSet {
-            textLabel?.text = item.text
+            if let toId = item.toId {
+                let ref = Database.database().reference().child("users").child(toId)
+                ref.observe(.value) { [weak self] (snapshot) in
+                    guard let this = self else { return }
+                    if let dictionary = snapshot.value as? [String: AnyObject] {
+                        this.titleLabel.text = dictionary["name"] as? String
+                    
+                        if let profileImageUrl = dictionary["profileImageUrl"] as? String {
+                            this.profileImageView.loadImageUsingCacheWithUrlString(urlString: profileImageUrl)
+                        }
+                    }
+                }
+            }
+            
+            descriptionLabel.text = item.text
         }
     }
 }
 
 final class MessageController: BaseTableView<MessageCell, Message> {
-    
-    //var message = Message()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -132,5 +180,9 @@ final class MessageController: BaseTableView<MessageCell, Message> {
         newMessageVC.messageController = self
         let naviController = UINavigationController(rootViewController: newMessageVC)
         present(naviController, animated: true, completion: nil)
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
     }
 }
