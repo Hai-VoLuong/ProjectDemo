921c962777ff4a248693c7b83029819423a514d5
haivl <hai.vl@techchain.vn>
2019-01-26 18:56:29 +0700

    init

---
+++ projectDemo/projectDemo/Controller/15LocalPushNotification/LocalPushNotificationController.swift
@@ -0,0 +1,18 @@
+//
+//  LocalPushNotificationController.swift
+//  projectDemo
+//
+//  Created by MAC on 1/26/19.
+//  Copyright © 2019 Hai Vo L. All rights reserved.
+//
+
+import UIKit
+
+final class LocalPushNotificationController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Local PushNotification"
+        view.backgroundColor = .white
+    }
+}
