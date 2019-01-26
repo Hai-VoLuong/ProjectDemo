61fe0e703ba008a87aca0bbce8359226278e864f
haivl <hai.vl@techchain.vn>
2019-01-26 19:33:13 +0700

    create request notification

--- projectDemo/projectDemo/Controller/15LocalPushNotification/LocalPushNotificationController.swift
+++ projectDemo/projectDemo/Controller/15LocalPushNotification/LocalPushNotificationController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 final class LocalPushNotificationController: UIViewController {
     
@@ -14,5 +15,25 @@ final class LocalPushNotificationController: UIViewController {
         super.viewDidLoad()
         title = "Local PushNotification"
         view.backgroundColor = .white
+        
+        testReceiveLocalNotification()
+    }
+    
+    /// định nghĩa một thời gian mà bạn sẽ phát ra thông báo.
+    /// Ví dụ ở trên là 5 giây sau khi bạn đặt hẹn phát.
+    /// Repeats là bạn muốn lặp lại thông báo không mà thôi.
+    /// ngoài ra: còn có
+    /// UNCalendarNotificationTrigger Trigger cho phép bạn hẹn ngày, thời gian để phát thông báo.
+    /// UNLocationNotificationTrigger Trigger địa điểm. Khi user đến một địa điểm nào đó thì sẽ có thông báo.
+    private func testReceiveLocalNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "Đây là cái title"
+        content.body = "Đây là cái nội dung."
+        content.sound = UNNotificationSound.default
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        // create request
+        let request = UNNotificationRequest(identifier: "Test", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request)
     }
 }
